cdae2142317c69fb56bc053366d290c64b82a6ac
duc2019 <52149933+duc2019@users.noreply.github.com>
2020-05-12 14:49:28 +0900

    Update saml2aws.rb

--- saml2aws.rb
+++ saml2aws.rb
@@ -2,13 +2,13 @@ require 'formula'
 
 class Saml2aws < Formula
   homepage 'https://github.com/lixiljp/saml2aws'
-  version '2.24.1'
+  version '2.26.1'
   if OS.mac?
-    url 'https://github.com/lixiljp/saml2aws/releases/download/eaa.20200213.2/saml2aws_2.24.1_darwin_amd64.tar.gz'
-    sha256 '574f030001fbbcbfefe9595ec7c4b0d2b7869dbe356b94859a1e50132e6aabe8'
+    url 'https://github.com/lixiljp/saml2aws/releases/download/eaa.20200512/saml2aws_2.26.1_darwin_amd64.tar.gz'
+    sha256 'f2cddf18899cfa3e51c9ebb5b75ef204cb2217e6003f1d91c39b07df4ac1596b'
   elsif OS.linux?
-    url 'https://github.com/lixiljp/saml2aws/releases/download/eaa.20200213.2/saml2aws_2.24.1_linux_amd64.tar.gz'
-    sha256 'c022a1c23a4d4d1ef658005dd2a458929a322018853b38e435e8c85115571fa0'
+    url 'https://github.com/lixiljp/saml2aws/releases/download/eaa.20200512/saml2aws_2.26.1_linux_amd64.tar.gz'
+    sha256 'c73652e2ebab6a71aee4a8c34f68968179b61d5b36c4aeac1c44d3ee2301778e'
   end
 
   depends_on :arch => :x86_64
