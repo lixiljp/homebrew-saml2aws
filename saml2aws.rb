66723955af614efc05a386001f6ffdd28de3f8a8
duc2019 <52149933+duc2019@users.noreply.github.com>
2021-08-27 13:57:24 +0900

    Update saml2aws.rb

--- saml2aws.rb
+++ saml2aws.rb
@@ -2,16 +2,16 @@ require 'formula'
 
 class Saml2aws < Formula
   homepage 'https://github.com/lixiljp/saml2aws'
-  version '2.26.3'
-  if OS.mac?
-    url 'https://github.com/lixiljp/saml2aws/releases/download/eaa.20200525/saml2aws_2.26.3_darwin_amd64.tar.gz'
-    sha256 '644743d1a12026391a502b26a9bfdfe417a9cf3af0d939c1239468d01816b448'
-  elsif OS.linux?
-    url 'https://github.com/lixiljp/saml2aws/releases/download/eaa.20200525/saml2aws_2.26.3_linux_amd64.tar.gz'
-    sha256 '20306f42163a0bf2269fb83d4649f4ce068f4c6465aa68388faf24cc624b74c2'
-  end
+  version '2.28.0'
 
-  depends_on :arch => :x86_64
+  bottle do
+    root_url "https://github.com/lixiljp/saml2aws/releases/download/eaa.20210827/"
+    sha256 cellar: "saml2aws_darwin_arm64.tar.gz", arm64_big_sur: "d15bda7fb656ef06365cb83ff31a9ea84884d1eeaef41b9c0180bd3b23632c50"
+    sha256 cellar: "saml2aws_darwin_amd64.tar.gz", big_sur:       "8d3db1a80ba8d51f9d5c65a556200547f18622793d12a3a3e842e16632412d50"
+    sha256 cellar: "saml2aws_darwin_amd64.tar.gz", catalina:      "8d3db1a80ba8d51f9d5c65a556200547f18622793d12a3a3e842e16632412d50"
+    sha256 cellar: "saml2aws_darwin_amd64.tar.gz", mojave:        "8d3db1a80ba8d51f9d5c65a556200547f18622793d12a3a3e842e16632412d50"
+    sha256 cellar: "saml2aws_linux_amd64.tar.gz",  x86_64_linux:  "e45c744ff6f13532ffefc285d5db481922faca3dead1beae7355428d8a8970cc"
+  end
 
   def install
     bin.install 'saml2aws'
