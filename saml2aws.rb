d61ff4e679016b81519ac766540ea8faca5416ce
duc2019 <52149933+duc2019@users.noreply.github.com>
2022-01-31 15:56:16 +0900

    Update saml2aws.rb

--- saml2aws.rb
+++ saml2aws.rb
@@ -6,15 +6,15 @@ class Saml2aws < Formula
 
   if OS.mac?
     if `uname -m`.strip == "arm64"
-      url 'https://github.com/lixiljp/saml2aws/releases/download/eaa.20210827/saml2aws_darwin_arm64.tar.gz'
-      sha256 'd15bda7fb656ef06365cb83ff31a9ea84884d1eeaef41b9c0180bd3b23632c50'
+      url 'https://github.com/lixiljp/saml2aws/releases/download/eaa.20220131/saml2aws_darwin_arm64.tar.gz'
+      sha256 'a2a844b1a285d6502635338c83db209319b1428af2c925759a39d9154bff36b2'
     else
-      url 'https://github.com/lixiljp/saml2aws/releases/download/eaa.20210827/saml2aws_darwin_amd64.tar.gz'
-      sha256 '8d3db1a80ba8d51f9d5c65a556200547f18622793d12a3a3e842e16632412d50'
+      url 'https://github.com/lixiljp/saml2aws/releases/download/eaa.20220131/saml2aws_darwin_amd64.tar.gz'
+      sha256 'f05140f80ea3c9a454284d0ce272324d41725891894ec3ea8fe44c80775f69f3'
     end
   elsif OS.linux?
-    url 'https://github.com/lixiljp/saml2aws/releases/download/eaa.20210827/saml2aws_linux_amd64.tar.gz'
-    sha256 'e45c744ff6f13532ffefc285d5db481922faca3dead1beae7355428d8a8970cc'
+    url 'https://github.com/lixiljp/saml2aws/releases/download/eaa.20220131/saml2aws_linux_amd64.tar.gz'
+    sha256 '34ffd2d571efdae1bbd41053278ae8998c11e840ede26d8e5abf0e4761447c8c'
   end
 
   def install
